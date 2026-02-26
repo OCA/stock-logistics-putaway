OCA/stock-logistics-warehouse

[18.0][ADD] stock_putaway_rule_product_handle

stock_putaway_rule_product_handle/__manifest__.py
@@ -1,0 +1,14 @@
+# Copyright 2025 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Putaway Rule Product Handle",
+    "summary": """This module allows to show the handle widget on
+    product putaway rules to change the sequence""",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "maintainers": ["rousseldenis"],
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "depends": ["stock"],
+}

OCA/stock-logistics-putaway

stock_putaway_rule_product_handle/__manifest__.py
@@ -1,0 +1,14 @@
+# Copyright 2025 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Putaway Rule Product Handle",
+    "summary": """This module allows to show the handle widget on
+    product putaway rules to change the sequence""",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "maintainers": ["rousseldenis"],
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-putaway",
+    "depends": ["stock"],
+}